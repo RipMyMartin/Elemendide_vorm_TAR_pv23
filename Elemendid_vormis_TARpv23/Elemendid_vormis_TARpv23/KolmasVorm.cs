d174138d3df4396c11d2cefe16d4cd19c5e996ab
RipMyMartin <161806791+RipMyMartin@users.noreply.github.com>
2024-10-09 11:42:17 +0300

    Add files via upload

---
+++ Elemendid_vormis_TARpv23/Elemendid_vormis_TARpv23/KolmasVorm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elemendid_vormis_TARpv23
+{
+    public partial class KolmasVorm : Form
+    {
+        Button btn;
+        public KolmasVorm(int h,int w)
+        {
+            this.Width= w;
+            this.Height= h;
+
+            this.Text = "Kolmas vorm";
+
+            btn = new Button();
+            btn.Text = "Vajuta siia";
+            btn.Height = 50;
+            btn.Width = 70;
+            btn.Location = new Point(150, 50);
+            btn.Click += Btn_Click;
+            Controls.Add(btn);
+        }
+
+        int t = 0;
+        private void Btn_Click(object? sender, EventArgs e)
+        {
+            t++;
+            if (t % 2 == 0)
+            {
+                btn.BackColor = Color.Red;
+            }
+            else
+            {
+                btn.BackColor = Color.Green;
+            }
+        }
+    }
+}
